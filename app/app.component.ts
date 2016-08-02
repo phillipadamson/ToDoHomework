78cd0598c32f7f473bded7da2bd5bcbe09c3b5fe
Phillip Adamson <padamson@avantlink.com>
2016-08-01 18:35:35 -0600

    This adds the tab interface, though it isn't complete and functionaly it isn't working.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,15 @@ import { AddToDoComponent } from './add-to-do.component';
 @Component({
   selector: 'my-app',
   template: `<nav-bar></nav-bar>
+             <div class="row">
+             <div class="col-xs-2">
+             </div>
+             <div class="col-xs-8 tab-body">
              <router-outlet></router-outlet>
+             </div>
+             <div class="col-xs-2">
+             </div>
+             </div>
              <av-footer></av-footer>`,
   directives: [NavBarComponent, AvFooterComponent, ROUTER_DIRECTIVES],
   precompile: [ListToDoComponent, AddToDoComponent]
