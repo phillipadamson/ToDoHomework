58c1d0f46578e6ca50d661149b8fac14e2694a17
Phillip Adamson <padamson@avantlink.com>
2016-08-03 15:54:50 -0600

    This finishes the functionality on the tab navigation.

--- app/nav-bar.component.ts
+++ app/nav-bar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: 'app/templates/nav-bar.component.html',
-  directives: [ROUTER_DIRECTIVES]
+    selector: 'nav-bar',
+    templateUrl: 'app/templates/nav-bar.component.html',
+    directives: [ROUTER_DIRECTIVES, NgClass]
 })
 
-export class NavBarComponent{
+export class NavBarComponent {
+    arrTab: any[] = [{ blnIsActive: true }, { blnIsActive: false }];
+    toggle(intIndex: number) {
+        if (this.arrTab[intIndex].blnIsActive === false) {
+          for(let i = 0; i < this.arrTab.length; i++) {
+            if(i === intIndex) {
+              this.arrTab[i].blnIsActive = true;
+            } else {
+              this.arrTab[i].blnIsActive = false;
+            }
+          }
+        }
 
+    }
 }
